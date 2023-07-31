2f49273bedd4fd8607cd59c754c0b306031b762e
FacundoBraga <facundobraga@hotmail.com>
2023-07-31 19:42:26 -0300

    Tipografia + Navbar Reestilizada + Inicio google reubicado

--- FRONT-END/CeiboApp/src/components/ui/Navbar.jsx
+++ FRONT-END/CeiboApp/src/components/ui/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Navbar } from "@nextui-org/react";
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -7,7 +8,7 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import { getUserByToken, userMe } from "../../utils/api";
 import { envs } from "../../config/env/env.config";
 
-const Navbar = () => {
+export default function NavBar() {
   const [userE, setUsere] = useState(null);
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -46,133 +47,96 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container col-12">
-        <Link className="navbar-brand" to="/">
-          CeiboApp
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="collapse navbar-collapse justify-content-between"
-          id="navbarNav"
-        >
-          <ul className="navbar-nav ms-auto">
-            {navbarOptions.map(
-              (option, index) =>
-                userE?.email &&
-                option.role.includes(userE.role) && (
-                  <li key={index} className={`nav-item `}>
-                    <Link to={option.path} className="nav-link">
-                      {option.label}
-                    </Link>
-                  </li>
-                )
-            )}
-
-            {!userE?.email && (
-              <li className="nav-item">
-                <Link to="/Register" className="nav-link">
-                  Register
-                </Link>
-              </li>
-            )}
-            {/* 
-            
-              *** CODIGO VIEJO POR LAS DUDAS ***
-
-            {userE?.role === "manager" ||
-              (userE?.role === "consultor" && (
-                <li className="nav-item">
-                  <Link to="/Projects" className="nav-link">
-                    {userE?.role === "manager" ? "Proyectos" : "Inicio"}
-                  </Link>
-                </li>
-              ))}
-            {userE?.email ? (
-              <>
-                {userE?.role === "admin" && (
-                  <>
-                    <li className="nav-item">
-                      <Link to="/admin/members" className="nav-link">
-                        Administrar Miembros
-                      </Link>
-                    </li>
-                  </>
-                )}
-                {userE?.role === "manager" && (
-                  <>
-                    <li className="nav-item">
-                      <Link to="/Projects" className="nav-link">
-                        Projectos
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link to="/partners" className="nav-link">
-                        Socios
-                      </Link>
-                    </li>
-                  </>
-                )}
-                {userE?.role === "consultor" && (
-                  <>
-                    <li className="nav-item">
-                      <Link to="/home" className="nav-link">
-                        Proyectos
-                      </Link>
-                    </li>
-                  </>
-                )}
-                {userE?.role === "socio" && (
-                  <>
-                    <li>
-                      <Link to="/home" className="nav-link">
-                        Mis Proyectos
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/home" className="nav-link">
-                        Ver Novedades
-                      </Link>
-                    </li>
-                  </>
-                )}
-              </>
-            ) : (
-              <li className="nav-item">
+    <Navbar isBordered variant="sticky">
+      <Navbar.Brand>
+        <img src="/ceibo-red.png" style={{ height: "45px" }} alt="" />
+      </Navbar.Brand>
+      <Navbar.Content enableCursorHighlight hideIn="xs" variant="underline">
+        {navbarOptions.map(
+          (option, index) =>
+            userE?.email &&
+            option.role.includes(userE.role) && (
+              <Navbar.Item key={option.label}>
                 <Link
-                  to={isLogin ? "/register" : "/login"}
-                  className="nav-link"
+                  color="inherit"
+                  css={{
+                    minWidth: "100%",
+                  }}
+                  to={`${option.path}`}
                 >
-                  {isLogin ? "Register" : "Login"}
+                  {option.label}
                 </Link>
-              </li>
-            )} */}
-          </ul>
-          <div>
-            {userE?.email ? (
-              <button className="btn btn-danger" onClick={handleLogout}>
-                Logout
-              </button>
-            ) : (
-              <Link to="/">
-                <button className="btn btn-warning">Login</button>
+              </Navbar.Item>
+            )
+        )}
+      </Navbar.Content>
+      <Navbar.Content className="mx-4">
+        {!userE?.email && (
+          <>
+            <Link to="/login">
+              <Navbar.Item color="inherit" className="d-none d-md-block">
+                <button type="button" class="btn btn-outline-danger">
+                  Iniciar sesión
+                </button>
+              </Navbar.Item>
+            </Link>
+            <Link to="/register">
+              <Navbar.Item className="d-none d-md-block">
+                <button type="button" class="btn btn-outline-danger">
+                  Registrarse
+                </button>
+              </Navbar.Item>
+            </Link>
+          </>
+        )}
+        {userE?.email && (
+          <button
+            className="btn btn-danger d-none d-md-block"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        )}
+        <Navbar.Toggle showIn="sm" aria-label="toggle navigation" />
+      </Navbar.Content>
+      <Navbar.Collapse>
+        {navbarOptions.map(
+          (option, index) =>
+            userE?.email &&
+            option.role.includes(userE.role) && (
+              <Navbar.CollapseItem key={option.label}>
+                <Link
+                  color="inherit"
+                  css={{
+                    minWidth: "100%",
+                  }}
+                  to={`${option.path}`}
+                >
+                  {option.label}
+                </Link>
+              </Navbar.CollapseItem>
+            )
+        )}
+        {!userE?.email && (
+          <>
+            <li className="nav-item">
+              <Link to="/register" className="nav-link">
+                Registrarse
               </Link>
-            )}
-          </div>
-        </div>
-      </div>
-    </nav>
+            </li>
+            <li className="nav-item">
+              <Link to="/login" className="nav-link">
+                Iniciar sesión
+              </Link>
+            </li>
+          </>
+        )}
+        {userE?.email && (
+          <button className="btn btn-danger" onClick={handleLogout}>
+            Logout
+          </button>
+        )}
+      </Navbar.Collapse>
+    </Navbar>
   );
-};
-
-export default Navbar;
+}
