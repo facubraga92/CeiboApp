2f6a73e2995d5a844fb709d2561db39fcbd3656c
BenjaLV <benjalavalla8@gmail.com>
2023-08-03 01:40:28 -0300

    vista partners y costumers

--- FRONT-END/CeiboApp/src/pages/AccountValidationMessage.jsx
+++ FRONT-END/CeiboApp/src/pages/AccountValidationMessage.jsx
@@ -3,17 +3,18 @@ import Layout from "../components/layouts/Layout";
 
 const AccountValidationMessage = () => {
   return (
-    <>
-    <Layout/>
-    <div className="container">
-      <h1>Valida tu cuenta</h1>
-      <p className="mt-4 fs-1 fw-bold">
-        Es necesario que valides tu cuenta para poder acceder al sitio. Por
-        favor, revisa tu correo electrónico y sigue las instrucciones para
-        completar la validación.
-      </p>
-    </div>
-    </>
+    <Layout>
+      <>
+        <div className="container">
+          <h1>Valida tu cuenta</h1>
+          <p className="mt-4 fs-1 fw-bold">
+            Es necesario que valides tu cuenta para poder acceder al sitio. Por
+            favor, revisa tu correo electrónico y sigue las instrucciones para
+            completar la validación.
+          </p>
+        </div>
+      </>
+    </Layout>
   );
 };
 
